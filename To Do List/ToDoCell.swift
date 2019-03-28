ad07121edc68de1df8f8b821e23558ed58cc27ed
Alshohatee <alshjohateeaseel@gmail.com>
2019-03-28 13:42:00 -0400

    Initial commit

---
+++ To Do List/ToDoCell.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Aseel Alshohatee on 3/27/19.
+//  Copyright © 2019 Aseel Alshohatee. All rights reserved.
+//
+
+import UIKit;
+
+protocol ToDoCellDelegate: class {
+    func checkmarkTapped(sender: ToDoCell);
+}
+
+class ToDoCell: UITableViewCell {
+    var delegate: ToDoCellDelegate? = nil;
+    @IBOutlet weak var isCompleteButton: UIButton!;
+    @IBOutlet weak var titleLabel: UILabel!;
+    
+    override func awakeFromNib() {
+        super.awakeFromNib();
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated);
+
+        // Configure the view for the selected state.
+    }
+    
+    @IBAction func completeButtonTapped(_ sender: UIButton) {  
+        delegate?.checkmarkTapped(sender: self);
+    }
+}
---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+// This program is made with the correction that you said.
+//  Created by Aseel Alshohatee on 3/27/19.
+//  Copyright © 2019 Aseel Alshohatee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
